aa824efda63ff0e7db110d02f8cb0894d3cc6cdb
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-08 16:17:43 +0300

    🔨 Add employee controller

---
+++ src/AuthGuard.Api/Controllers/v1/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using EasyWeb.AspNetCore.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthGuard.Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Authorize]
+    [Route("v{ver:apiVersion}/employees")]
+    public class EmployeeController : BaseApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test");
+        }
+    }
+}
