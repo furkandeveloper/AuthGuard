96b48a34020592087521269bafdaf9ce09dd83ab
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-08 14:06:47 +0300

    🔨 Add scopes for identity server

---
+++ src/AuthGuard.IdentityServer4/Helpers/Scopes.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.Models;
+
+namespace AuthGuard.IdentityServer4.Helpers
+{
+    public class Scopes
+    {
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("employeeApi.read", "Read Access to Employee API"),
+                new ApiScope("employeeApi.create", "Create Access to Employee API"),
+                new ApiScope("employeeApi.update", "Update Access to Employee API"),
+                new ApiScope("employeeApi.delete", "Delete Access to Employee API"),
+            };
+        }
+    }
+}
