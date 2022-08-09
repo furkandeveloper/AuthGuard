6d7d70c45f992a6cae896ece37ad78852991b792
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-09 12:28:42 +0300

    🔨 Add employee entity

---
+++ src/AuthGuard.Domain/Employee.cs
@@ -0,0 +1,34 @@
+﻿using EasyRepository.EFCore.Abstractions;
+
+namespace AuthGuard.Domain
+{
+    /// <summary>
+    /// Employee Entity
+    /// </summary>
+    [Serializable]
+    public class Employee : EasyBaseEntity<Guid>
+    {
+        public Employee(string firstName, string lastName, int age)
+        {
+            ArgumentNullException.ThrowIfNull(firstName);
+            ArgumentNullException.ThrowIfNull(lastName);
+            ArgumentNullException.ThrowIfNull(age);
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void Update(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
