1122b1ff9d9faad8b492c053b8a10d475f30183f
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-09 12:29:13 +0300

    🔨 Add Guid value genarator

---
+++ src/AuthGuard.EntityFrameworkCore/ValueGenerators/GuidValueGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace AuthGuard.EntityFrameworkCore.ValueGenerators;
+
+public class GuidValueGenerator : ValueGenerator
+{
+    public override bool GeneratesTemporaryValues => false;
+
+    protected override object NextValue(EntityEntry entry)
+    {
+        return Guid.NewGuid();
+    }
+
+}
