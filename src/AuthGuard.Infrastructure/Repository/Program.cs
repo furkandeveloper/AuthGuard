ec64aeeaf01558886924cc4de4f30c273e94c60c
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-09 12:29:02 +0300

    🔨 Add Easy Repository initializer extension method

---
+++ src/AuthGuard.Infrastructure/Repository/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthGuard.Infrastructure.Repository
+{
+    public static class Program
+    {
+        public static void ApplyRepository<TDbContext>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Transient)
+            where TDbContext : DbContext
+        {
+            services.ApplyRepository<TDbContext>(lifetime);
+        }
+    }
+}
