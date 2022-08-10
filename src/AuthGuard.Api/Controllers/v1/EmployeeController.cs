a2ff418ae7baf77c825d46710f4f3ceecc69ac3a
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 12:27:43 +0300

    🔨 Implement advanced filter endpoint

--- src/AuthGuard.Api/Controllers/v1/EmployeeController.cs
+++ src/AuthGuard.Api/Controllers/v1/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using EasyWeb.AspNetCore.Controllers;
+﻿using AuthGuard.Application.Dtos;
+using AuthGuard.Application.Services.Abstractions;
+using EasyWeb.AspNetCore.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +11,30 @@ namespace AuthGuard.Api.Controllers.v1
     [Route("v{ver:apiVersion}/employees")]
     public class EmployeeController : BaseApiController
     {
-        [HttpGet]
-        public IActionResult Get()
+        private readonly IEmployeeApplicationService employeeApplicationService;
+
+        public EmployeeController(IEmployeeApplicationService employeeApplicationService)
         {
-            return Ok("Test");
+            this.employeeApplicationService = employeeApplicationService;
+        }
+
+        /// <summary>
+        /// Advanced Filter for Employee
+        /// </summary>
+        /// <param name="model">
+        /// Employee Filter Data Transfer Object. <see cref="EmployeeFilterDto"/>
+        /// </param>
+        /// <returns>
+        /// List of Employee Response Data Transfer Object. <see cref="List{EmployeeResponseDto}"/>
+        /// </returns>
+        [HttpGet(Name = "AdvancedFilter")]
+        [ProducesResponseType(typeof(EmployeeResponseDto[]), 200)]
+        public async Task<IActionResult> AdvancedFilterAsync([FromQuery] EmployeeFilterDto model)
+        {
+            return Ok(await employeeApplicationService.FilterAsync(model), new Dictionary<string, int>
+            {
+                { "TotalCount",await employeeApplicationService.CountAsync() }
+            });
         }
     }
 }
