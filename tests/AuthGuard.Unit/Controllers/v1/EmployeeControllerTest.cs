b0c9f22460162ca45c04ae62627ed4e8416fe9c6
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 15:09:36 +0300

    ✅ Add test cases for find employee endpoint

--- tests/AuthGuard.Unit/Controllers/v1/EmployeeControllerTest.cs
+++ tests/AuthGuard.Unit/Controllers/v1/EmployeeControllerTest.cs
@@ -183,27 +183,27 @@ namespace AuthGuard.Unit.Controllers.v1
             // Arrange
 
             var sut = new EmployeeController(service.Object);
-            service.Setup(setup => setup.UpdateAsync(actualId,actual)).ReturnsAsync(expected);
+            service.Setup(setup => setup.UpdateAsync(actualId, actual)).ReturnsAsync(expected);
 
             // Act
 
-            var result = await sut.UpdateAsync(actualId,actual);
+            var result = await sut.UpdateAsync(actualId, actual);
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
         }
 
         [Theory, AutoMoqData]
-        public async Task Update_Employee_Return_Employee_Response(Mock<IEmployeeApplicationService> service, EmployeeRequestDto actual, Guid actualId,EmployeeResponseDto expected)
+        public async Task Update_Employee_Return_Employee_Response(Mock<IEmployeeApplicationService> service, EmployeeRequestDto actual, Guid actualId, EmployeeResponseDto expected)
         {
             // Arrange
 
             var sut = new EmployeeController(service.Object);
-            service.Setup(setup => setup.UpdateAsync(actualId,actual)).ReturnsAsync(expected);
+            service.Setup(setup => setup.UpdateAsync(actualId, actual)).ReturnsAsync(expected);
 
             // Act
 
-            var result = await sut.UpdateAsync(actualId,actual);
+            var result = await sut.UpdateAsync(actualId, actual);
 
             // Assert
             var apiOkResult = result.Should().BeOfType<OkObjectResult>().Subject;
@@ -234,7 +234,7 @@ namespace AuthGuard.Unit.Controllers.v1
             // Arrange
 
             var sut = new EmployeeController(null);
-            service.Setup(setup => setup.UpdateAsync(actualId,actual)).ReturnsAsync(expected);
+            service.Setup(setup => setup.UpdateAsync(actualId, actual)).ReturnsAsync(expected);
 
             // Act
 
@@ -244,7 +244,7 @@ namespace AuthGuard.Unit.Controllers.v1
             await action.Should().ThrowAsync<NullReferenceException>();
         }
 
-        [Theory,AutoMoqData]
+        [Theory, AutoMoqData]
         public async Task Delete_Employee_No_Content_Result(Mock<IEmployeeApplicationService> service, Guid actual, Task expected)
         {
             // Arrange
@@ -292,5 +292,70 @@ namespace AuthGuard.Unit.Controllers.v1
             // Assert
             await action.Should().ThrowAsync<NullReferenceException>();
         }
+
+        [Theory, AutoMoqData]
+        public async Task Find_Employee_Return_Ok_Object_Result(Mock<IEmployeeApplicationService> service, Guid actual, EmployeeResponseDto expected)
+        {
+            // Arrange
+            
+            var sut = new EmployeeController(service.Object);
+            service.Setup(setup => setup.FindAsync(actual)).ReturnsAsync(expected);
+
+            // Act
+            var result = await sut.FindAsync(actual);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task Find_Employee_Return_Employee_Response(Mock<IEmployeeApplicationService> service, Guid actual, EmployeeResponseDto expected)
+        {
+            // Arrange
+
+            var sut = new EmployeeController(service.Object);
+            service.Setup(setup => setup.FindAsync(actual)).ReturnsAsync(expected);
+
+            // Act
+            var result = await sut.FindAsync(actual);
+
+            // Assert
+            var apiOkResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiOkResult.Value);
+            var response = model.Data;
+            Assert.IsType<EmployeeResponseDto>(response);
+        }
+
+
+        [Theory, AutoMoqData]
+        public async Task Find_Employee_Not_Throw(Mock<IEmployeeApplicationService> service, Guid actual, EmployeeResponseDto expected)
+        {
+            // Arrange
+
+            var sut = new EmployeeController(service.Object);
+            service.Setup(setup => setup.FindAsync(actual)).ReturnsAsync(expected);
+
+            // Act
+            var action = async () => { await sut.FindAsync(actual); };
+
+            // Assert
+            await action.Should().NotThrowAsync<Exception>();
+        }
+
+        [Theory, AutoMoqData]
+        public async Task Find_Employee_Throw(Mock<IEmployeeApplicationService> service, Guid actual, EmployeeResponseDto expected)
+        {
+            // Arrange
+
+            var sut = new EmployeeController(null);
+            service.Setup(setup => setup.FindAsync(actual)).ReturnsAsync(expected);
+
+            // Act
+            var action = async () => { await sut.FindAsync(actual); };
+
+            // Assert
+            await action.Should().ThrowAsync<NullReferenceException>();
+        }
+
     }
 }
