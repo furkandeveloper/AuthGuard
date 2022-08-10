c9cd13a72a7d8250334e003b481abbd948a172dd
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 10:43:20 +0300

    🔨 Add Employee Request Data Transfer Object

---
+++ src/AuthGuard.Application/Dtos/EmployeeRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthGuard.Application.Dtos;
+
+public class EmployeeRequestDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+}
