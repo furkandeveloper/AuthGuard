322b8a947b19b35cfe57a7deeedfb099ea14a549
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 10:43:05 +0300

    🔨 Add object mapping rule for employee

---
+++ src/AuthGuard.Application/Mapping/EmployeeMap.cs
@@ -0,0 +1,13 @@
+﻿using AuthGuard.Application.Dtos;
+using AuthGuard.Domain;
+using AutoMapper;
+
+namespace AuthGuard.Application.Mapping;
+
+public class EmployeeMap : Profile
+{
+    public EmployeeMap()
+    {
+        CreateMap<Employee, EmployeeResponseDto>();
+    }
+}
