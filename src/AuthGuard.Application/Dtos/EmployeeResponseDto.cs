c4a800da7b5dabba1bfc88966b2793c617eefc99
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 10:43:37 +0300

    🔨 Add Employee Response Data Transfer Object

---
+++ src/AuthGuard.Application/Dtos/EmployeeResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthGuard.Application.Dtos;
+
+public class EmployeeResponseDto : BaseResponseDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+}
