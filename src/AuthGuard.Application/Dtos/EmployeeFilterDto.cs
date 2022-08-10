9e36123bcb3aa18c0b05f594b16f4fa0e2a57060
furkandeveloper <furkan.dvlp@gmail.com>
2022-08-10 10:42:32 +0300

    🔨 Add Employee Filter Data Transfer Object

---
+++ src/AuthGuard.Application/Dtos/EmployeeFilterDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+
+namespace AuthGuard.Application.Dtos;
+
+[PossibleSortings("FirstName","LastName","Age","CreationDate")]
+public class EmployeeFilterDto : PaginationFilterBase
+{
+    [ToLowerContainsComparison]
+    public string FirstName { get; set; }
+    
+    [ToLowerContainsComparison]
+    public string LastName { get; set; }
+    
+    public Range<int> Age { get; set; }
+
+    public Range<DateTime> CreationDate { get; set; }
+}
